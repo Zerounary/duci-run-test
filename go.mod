de7c56d1983f291b593acfb7bd3407ba2409866f
Zerounary <850657663@qq.com>
2020-11-24 19:54:08 +0800

    use third pkg ajson

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Zerounary/duci-run-test
 
 go 1.15
+
+require github.com/spyzhov/ajson v0.4.2
--- main.go
+++ main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"log"
 	"os"
+
+	"github.com/spyzhov/ajson"
 )
 
 func main() {
+	json := []byte(`{"price": 1}`)
+	root, _ := ajson.Unmarshal(json)
+	nodes, _ := root.JSONPath("$..price")
+
 	file, err := os.Create("test555")
 	if err != nil {
 		log.Fatal(err)
 	}
-	file.WriteString("test555")
+	file.WriteString(nodes[0].String())
 	file.Close()
 }
